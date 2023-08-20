adbe2df850d27f7d951abcfcd6aecab7daed565f
Luis Wirth <lwirth2000@gmail.com>
2023-08-20 16:45:32 +0200

    nix flake support

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "pico-examples";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    lwirth-nixpkgs.url = "github:lu15w1r7h/nixpkgs/openocd-rp2040";
+  };
+
+  outputs = { self, nixpkgs, lwirth-nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      lwirth-pkgs = import lwirth-nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShell.${system} = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cmake
+          clang-tools
+          cmake-language-server
+
+          lwirth-pkgs.openocd-rp2040
+          gcc-arm-embedded
+          gdb
+          
+          minicom
+        ];
+
+        PICO_SDK_PATH = "/home/luis/dev/opensource/pico-sdk";
+        PICO_BOARD="pico_w";
+      };
+    };
+}
